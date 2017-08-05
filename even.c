a3bd8566a556bded102c412cbc9d520288fc2111
Monika19966 <30742880+Monika19966@users.noreply.github.com>
2017-08-05 10:56:23 +0530

    Create even.c

---
+++ even.c
@@ -0,0 +1,12 @@
+#include <stduio.h>
+
+void main()
+{
+int num;
+printf("enter a number \n");
+scanf("%", &number);
+if (num >=0)
+printf("%d is a positive number \n", num);
+else
+printf("%d is a negative number \n", num);
+}
